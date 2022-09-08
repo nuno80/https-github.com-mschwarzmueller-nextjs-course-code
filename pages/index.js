a339c5915e0c75d883efd07a3c9fe151d8ee8b74
nuno80 <104353267+nuno80@users.noreply.github.com>
2022-09-08 11:01:21 +0200

    1 file modified, 1 added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-ines (8 sloc)  181 Bytes
-
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Hello Next World!</h1>
     </div>
-  )
+  );
 }
